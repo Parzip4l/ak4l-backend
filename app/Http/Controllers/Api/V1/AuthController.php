6017f572725f77bd81a933d768f3e378f2dbef49
mochreza1407 <sobirinjamo33@gmail.com>
2025-12-03 23:56:03 +0700

    update

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -64,13 +64,16 @@ class AuthController extends Controller
 
         try {
             $connection = new \LdapRecord\Connection([
-                'hosts'    => [env('LDAP_HOST')],
-                'base_dn'  => env('LDAP_BASE_DN'),
+                
+                'hosts'    => config('ldap.connections.default.hosts'), 
+                
+                'base_dn'  => config('ldap.connections.default.base_dn'),
+                
                 'username' => null,
                 'password' => null,
-                'port'     => env('LDAP_PORT', 389),
-                'use_ssl'  => env('LDAP_SSL', false),
-                'use_tls'  => env('LDAP_TLS', false),
+                'port'     => config('ldap.connections.default.port', 389),
+                'use_ssl'  => config('ldap.connections.default.use_ssl', false),
+                'use_tls'  => config('ldap.connections.default.use_tls', false),
             ]);
 
             // Connect ke LDAP server
